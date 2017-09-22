db97b51e5cb3e7376469ca36ffaefe7033c972c6
Saurabh <msaurabh2013@gmail.com>
2017-09-22 00:08:34 -0400

    initial project repo

---
+++ iTutor/iTutor/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  iTutor
+//
+//  Created by Saurabh Mehta on 9/22/17.
+//  Copyright © 2017 rit. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
